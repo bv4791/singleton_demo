892ded6fc7d85a058de0bd3d561323b844e02365
bv4791 <bv4791@gmail.com>
2021-03-23 18:46:11 +0200

    Refactoring extract

--- app.rb
+++ app.rb
@@ -1,30 +1,5 @@
-class Logger
-  def initialize
-     @f = File.open 'log.txt', 'a'
-  end
+require './logger'
 
-  @@x = nil
-  def self.instance
-    if @@x == nil
-      @@x = Logger.new
-    end
-    return @@x 
-  end
-
-  def self.say_something
-    puts 'Haha'
-  end
- 
-  def log_something (wat)   
-    @f.puts wat
-  end 
-end
-
-Logger.say_something
-Logger.instance.log_something 'bla1'
-Logger.instance.log_something 'bla2'
-
-logger=Logger.new
-logger.log_something 'way'
+Logger.instance.log_something 'bla bla'
 
 
