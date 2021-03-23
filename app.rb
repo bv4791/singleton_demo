379fbe278df6ab52f77dce789737f6454f18b73c
bv4791 <bv4791@gmail.com>
2021-03-23 18:33:07 +0200

    Update logger

--- app.rb
+++ app.rb
@@ -3,6 +3,14 @@ class Logger
      @f = File.open 'log.txt', 'a'
   end
 
+  @@x = nil
+  def self.instance
+    if @@x == nil
+      @@x = Logger.new
+    end
+    return @@x 
+  end
+
   def self.say_something
     puts 'Haha'
   end
@@ -13,6 +21,9 @@ class Logger
 end
 
 Logger.say_something
+Logger.instance.log_something 'bla1'
+Logger.instance.log_something 'bla2'
+
 logger=Logger.new
 logger.log_something 'way'
 
